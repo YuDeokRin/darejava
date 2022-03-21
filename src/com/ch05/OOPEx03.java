9942b71899a5347e674bae8f388c5302e0332ea5
YuDeokRin <ejrfls12@naver.com>
2022-03-21 11:51:52 +0900

    ObjectEx01변경 - 상세한 설명 첨가

--- src/com/ch05/OOPEx03.java
+++ src/com/ch05/OOPEx03.java
@@ -10,12 +10,12 @@ class 홍길동 extends 요리사 {
 
 public class OOPEx03 {
     public static void main(String[] args) {
-    홍길동 n1 = new 홍길동();
+    홍길동 n1 = new 홍길동(); // 메모리(heap)에서 (홍길동, 요리사) 이렇게 있으면 홍길동을 가리킨다.  n1의 타입은 "홍길동"
         System.out.println(n1.name);
 
-    요리사 y1 = new 홍길동();
+    요리사 y1 = new 홍길동(); // 메모리(heap)에서 (홍길동, 요리사) 이렇게 있으면 요리사를 가리킨다.  y1의 타입은 "요리사"
         System.out.println(y1.name);
 
-
+    //홍길동 h2 = new 요리사(); // 메모리(haep)에서 (요리사) 이렇게만 있는 상황에서 홍길동의 타입을 사용할 수 없다.
     }
 }
--- src/com/ch06/ObjectEx01.java
+++ src/com/ch06/ObjectEx01.java
@@ -13,16 +13,16 @@ class Cat {
 public class ObjectEx01 {
     public static void main(String[] args) {
         Object a1 = new Dog(); // 다형성
-        Object a2 = new Cat(); //가능
+        Object a2 = new Cat(); //가능한 이유 : 모든 클래스는 Object를 상속하고 있다.  그래서 생략해서 extends Object를 잘 사용하지 않는다.
 
-        Dog d2 = (Dog) a1; // 다운 캐스팅
-        Cat c1 = (Cat) a2;
+        Dog d1 = (Dog) a1; // a1을 사용하려면 다운 캐스팅을 사용하여 쓸수 밖에없다. a1의 타입은 Object타입이다.
+        Cat c1 = (Cat) a2; // a2도 마찬가지이다.
 
         /*
         Cat c2 = (Cat)a1; // ClassCastException : 캐스팅 문제- 강아지의 오브젝트에서 다운캐스팅은 될수 없다.
         */
 
-        System.out.println(d2.name);
+        System.out.println(d1.name);
         System.out.println(c1.name );
 
     }
